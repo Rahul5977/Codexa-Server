db5f0a77f570549e2467de59eac54dc360ae7d3a
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2026-02-02 09:33:23 +0530

    feat: integrate Cloudinary for image upload functionality and add upload route

--- utils-service/app.ts
+++ utils-service/app.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import cors from 'cors'
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import utilsRoutes from "./routes.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+dotenv.config();
 
 const app = express();
 
 // Body parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors())
+
+app.use('/api/utils', utilsRoutes);
 
 // Health check endpoint
 app.get("/health", (_req, res) => {
